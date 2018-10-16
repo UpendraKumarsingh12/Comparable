0874ffcf15305df06e16b992dbf9afcfa27ee3c0
upendrakumarsingh12 <upendra.singh001989@gmail.com>
2018-10-16 19:32:03 +0530

    comparable

--- LogicalProgrammin/src/com/comarable/TestSort.java
+++ LogicalProgrammin/src/com/comarable/TestSort.java
@@ -1,5 +1,17 @@
 package com.comarable;
 
-public class TestSort {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class TestSort {
+public static void main(String[] args) {
+	ArrayList<Student>list= new ArrayList<>();
+	list.add(new Student(101,"Vijay",23));  
+	list.add(new Student(106,"Ajay",27));  
+	list.add(new Student(105,"Jai",21)); 
+	Collections.sort(list);
+	for(Student s:list) {
+		System.out.println(s);
+	}
+}
 }
