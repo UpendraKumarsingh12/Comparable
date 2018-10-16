274abde034339552b7b110bd308ec3603d173596
upendrakumarsingh12 <upendra.singh001989@gmail.com>
2018-10-16 19:33:57 +0530

    c

--- LogicalProgrammin/src/com/comarable/Student.java
+++ LogicalProgrammin/src/com/comarable/Student.java
@@ -12,6 +12,11 @@ public Student(int rollnum, String name, int age) {
 }
 
 @Override
+public String toString() {
+	return "Student [rollnum=" + rollnum + ", name=" + name + ", age=" + age + "]";
+}
+
+@Override
 public int compareTo(Student st) {
 	if(age==st.age) {
 		return 0;
