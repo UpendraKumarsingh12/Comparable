7c0c13107fb5f0a0d4cabdc3205f4ff3707a52d7
upendrakumarsingh12 <upendra.singh001989@gmail.com>
2018-10-16 19:27:51 +0530

    ggf

--- LogicalProgrammin/src/com/comarable/Student.java
+++ LogicalProgrammin/src/com/comarable/Student.java
@@ -1,5 +1,24 @@
 package com.comarable;
 
-public class Student {
+public class Student implements Comparable<Student> {
+int rollnum;
+String name;
+int age;
+public Student(int rollnum, String name, int age) {
+	super();
+	this.rollnum = rollnum;
+	this.name = name;
+	this.age = age;
+}
+
+@Override
+public int compareTo(Student st) {
+	if(age==st.age) {
+		return 0;
+	}else if(age>st.age) {
+		return 1;
+	}else
+	return -1;
+}
 
 }
